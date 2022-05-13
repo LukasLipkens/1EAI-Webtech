1ca311d79f15c79125dc5d16e8eda4aaeb2d070e
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2022-05-13 21:40:30 +0200

    other email test

--- Bootstrap/message.php
+++ Bootstrap/message.php
@@ -6,7 +6,7 @@
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "lukaslipkens@gmail.com"; //enter that email address where you want to receive all messages
+      $receiver = "12101747@student.pxl.be"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
       $body = "Name: $name\nEmail: $email\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";

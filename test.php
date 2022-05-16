fe1a1871d2238b866997cb4b3da4447508ecbecc
LukasLipkens <84568981+LukasLipkens@users.noreply.github.com>
2022-05-16 16:18:11 +0200

    1

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "dbuser";
-$password = "Stella-123";
+$password = "1234";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
